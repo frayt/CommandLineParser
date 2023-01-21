eb4d1073c40956e942f8c40caa05408ee66bee30
frayt <spiddlypip@hotmail.com>
2023-01-21 14:28:51 -0800

    Changes for updating to .netstandard2.1

--- Deploy.bat
+++ Deploy.bat
@@ -1,6 +1,13 @@
-nuget pack Dll.nuspec
+@echo off
+dotnet pack
 mkdir NugetOutput
-move /Y *.nupkg NugetOutput\output.nupkg
+
+echo The .nupkg file has been built!  Please copy it to NugetOuput, and rename it to output.nupkg
+echo Then press enter..
+pause
+
+rem move /Y CommandLineParser\bin\Debug\*.nupkg NugetOutput\output.nupkg
+
 nuget add NugetOutput\output.nupkg -source \\Beast\NugetRepo
 
 pause
