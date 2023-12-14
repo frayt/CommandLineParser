33ad3f492ebdcba5cd99591f21486cbf18e12f9c
frayt <spiddlypip@hotmail.com>
2023-12-13 17:11:32 -0800

    Added virtual GetHelpText(), plus some renaming/cleanup

--- CommandLineParser/ExpectedOption.cs
+++ CommandLineParser/ExpectedOption.cs
@@ -3,9 +3,9 @@ namespace Fray.CommandLineParser
 {
     /// <summary>
     /// This is a single option that you might expect to see in the command line args.
-    /// Ex: option:"help" for --help  shortOption:"h" for -h   'help' is a stand alone arg
+    /// Ex: Option:"help" for --help  ShortOption:"h" for -h   'help' is a stand alone arg
     ///  and so would be type "StandAlone"
-    /// Ex: option:"timeout" for --timeout  shortOption:"t" for -t   'timeout' would expect
+    /// Ex: Option:"timeout" for --timeout  ShortOption:"t" for -t   'timeout' would expect
     ///  one sub arg, so it's type would be "OneParam"
     /// </summary>
     public class ExpectedOption
@@ -13,15 +13,15 @@ namespace Fray.CommandLineParser
         /// <summary>
         /// Long version:  ex: help    so the command line arg would be --help
         /// </summary>
-        public readonly string option;
+        public readonly string Option;
         /// <summary>
         /// Short version:  ex: h    so the command line arg would be -h
         /// </summary>
-        public readonly string shortOption;
+        public readonly string ShortOption;
         /// <summary>
         /// This is how you specify if any sub args are expected on the option
         /// </summary>
-        public enum EType
+        public enum EOptionType
         {
             /// <summary>No sub args are expected on this option.  Ex: --help</summary>
             StandAlone,
@@ -33,7 +33,7 @@ namespace Fray.CommandLineParser
         /// <summary>
         /// Parameter type
         /// </summary>
-        public readonly EType type;
+        public readonly EOptionType OptionType;
 
         /// <summary>
         /// Construct an ExpectedOption object
@@ -41,6 +41,19 @@ namespace Fray.CommandLineParser
         /// <param name="option">Long name of option.  Ex: help</param>
         /// <param name="shortOption">Short name of option.  Ex: h</param>
         /// <param name="type">See EType enum for help</param>
-        public ExpectedOption(string option, string shortOption, EType type) { this.option = option; this.shortOption = shortOption; this.type = type; }
+        public ExpectedOption(string option, string shortOption, EOptionType type)
+        {
+            if (string.IsNullOrWhiteSpace(option))
+                throw new System.ArgumentNullException(nameof(option));
+            Option = option;
+            ShortOption = shortOption;
+            OptionType = type;
+        }
+
+        /// <summary>
+        /// Get a help string for this ExpectedOption
+        /// </summary>
+        /// <returns>Help string, or null if not implemented</returns>
+        public virtual string GetHelpText() => null;
     }
 }
