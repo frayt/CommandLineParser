a648323255fe8ad46571afeb000835c22c6bdb3e
frayt <spiddlypip@hotmail.com>
2023-12-13 17:12:33 -0800

    Add extra versions of the functions that take an ExpectedOption instead of just a string

--- CommandLineParser/Options.cs
+++ CommandLineParser/Options.cs
@@ -30,13 +30,33 @@ namespace Fray.CommandLineParser
         }
 
         /// <summary>
+        /// Does a simple check to see if the request option exists
+        /// </summary>
+        /// <param name="option">ExpectedOption you're checking against</param>
+        /// <returns>true if option exists</returns>
+        public bool OptionExists(ExpectedOption option)
+        {
+            return options.Find(o => o.name == option.Option) != null;
+        }
+
+        /// <summary>
         /// Get Option object by option name
         /// </summary>
-        /// <param name="option">long/full name of option</param>
+        /// <param name="option">long/full name of option you are matching against</param>
         /// <returns>Returns Option if exists, otherwise returns null</returns>
         public Option GetOption(string option)
         {
             return options.Find(o => o.name == option);
+        }
+
+        /// <summary>
+        /// Get Option object by ExpectedOption
+        /// </summary>
+        /// <param name="option">The ExpectedOption that you are matching against</param>
+        /// <returns>Returns Option if exists, otherwise returns null</returns>
+        public Option GetOption(ExpectedOption option)
+        {
+            return GetOption(option.Option);
         }
 
         /// <summary>
@@ -54,6 +74,18 @@ namespace Fray.CommandLineParser
             if (o.args.Count == 0)
                 return null;
             return o.args[0];
+        }
+
+        /// <summary>
+        /// If an option expects a single sub arg, this retrieves it.
+        /// Ex: If your tool's Copy command looked like this: "Copy srcFile destFile --timeout 30"
+        ///  GetOptionSingleArg("timeout") would return "30"
+        /// </summary>
+        /// <param name="option">long/full name of option</param>
+        /// <returns>The sub arg</returns>
+        public string GetOptionSingleArg(ExpectedOption option)
+        {
+            return GetOptionSingleArg(option.Option);
         }
     }
 }
