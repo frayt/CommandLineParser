3e8929f622f1b7d8393ca1892006bb20cc80829b
frayt <spiddlypip@hotmail.com>
2023-12-13 20:43:20 -0800

    Fix up tests and add ExampleUsage

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -1,6 +1,7 @@
 ﻿
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Fray.CommandLineParser;
+using System.ComponentModel.Design;
 
 namespace UnitTests
 {
@@ -12,28 +13,28 @@ namespace UnitTests
         {
             var expectedOptions = new ExpectedOption[]
             {
-                new ExpectedOption("help", "h", ExpectedOption.EType.StandAlone),
-                new ExpectedOption("retries", "r", ExpectedOption.EType.OneParam),
-                new ExpectedOption("settings", "s", ExpectedOption.EType.OneParam),
-                new ExpectedOption("randomNumbers", "rn", ExpectedOption.EType.OneOrMoreParams),
+                new ExpectedOption("help", "h", ExpectedOption.EOptionType.StandAlone),
+                new ExpectedOption("retries", "r", ExpectedOption.EOptionType.OneParam),
+                new ExpectedOption("settings", "s", ExpectedOption.EOptionType.OneParam),
+                new ExpectedOption("randomNumbers", "rn", ExpectedOption.EOptionType.OneOrMoreParams),
             };
 
             string[] args;
-            Options options;
+            ParsedOptions options;
 
             // Single option tests:
 
             args = new string[] { "--help" };
             options = Parser.Parse(args, expectedOptions);
-            Assert.AreEqual(1, options.options.Count);
-            Assert.AreEqual(0, options.standAloneArgs.Count);
-            Assert.AreEqual("help", options.options[0].name);
-            Assert.AreEqual(0, options.options[0].args.Count);
+            Assert.AreEqual(1, options.Options.Count);
+            Assert.AreEqual(0, options.StandAloneArgs.Count);
+            Assert.AreEqual("help", options.Options[0].Name);
+            Assert.AreEqual(0, options.Options[0].Args.Count);
 
             args = new string[] { "-h" };
             options = Parser.Parse(args, expectedOptions);
-            Assert.AreEqual(1, options.options.Count);
-            Assert.AreEqual(0, options.standAloneArgs.Count);
+            Assert.AreEqual(1, options.Options.Count);
+            Assert.AreEqual(0, options.StandAloneArgs.Count);
 
             args = new string[] { "-help" }; // -help doesn't exist!
             Assert.ThrowsException<ParseException>(() => Parser.Parse(args, expectedOptions));
@@ -45,16 +46,16 @@ namespace UnitTests
 
             args = new string[] { "srcFile", "destFile" };
             options = Parser.Parse(args, expectedOptions);
-            Assert.AreEqual(2, options.standAloneArgs.Count);
-            Assert.AreEqual(0, options.options.Count);
+            Assert.AreEqual(2, options.StandAloneArgs.Count);
+            Assert.AreEqual(0, options.Options.Count);
 
             args = new string[] { "srcFile", "destFile", "--retries", "5" };
             options = Parser.Parse(args, expectedOptions);
-            Assert.AreEqual(2, options.standAloneArgs.Count);
-            Assert.AreEqual(1, options.options.Count);
-            Assert.AreEqual("retries", options.options[0].name);
-            Assert.AreEqual(1, options.options[0].args.Count);
-            Assert.AreEqual("5", options.options[0].args[0]);
+            Assert.AreEqual(2, options.StandAloneArgs.Count);
+            Assert.AreEqual(1, options.Options.Count);
+            Assert.AreEqual("retries", options.Options[0].Name);
+            Assert.AreEqual(1, options.Options[0].ArgsCount);
+            Assert.AreEqual("5", options.Options[0].Args[0]);
 
             // Only 1 param fail:
 
@@ -64,12 +65,23 @@ namespace UnitTests
             // N params test:
             args = new string[] { "-h", "-rn", "1", "2", "3", "-r", "100" };
             options = Parser.Parse(args, expectedOptions);
-            Assert.AreEqual(0, options.standAloneArgs.Count);
-            Assert.AreEqual(3, options.options.Count);
-            Assert.AreEqual("help", options.options[0].name);
-            Assert.AreEqual("randomNumbers", options.options[1].name);
-            Assert.AreEqual("retries", options.options[2].name);
-            Assert.AreEqual(3, options.options[1].args.Count);
+            Assert.AreEqual(0, options.StandAloneArgs.Count);
+            Assert.AreEqual(3, options.Options.Count);
+            Assert.AreEqual("help", options.Options[0].Name);
+            Assert.AreEqual("randomNumbers", options.Options[1].Name);
+            Assert.AreEqual("retries", options.Options[2].Name);
+            Assert.AreEqual(3, options.Options[1].Args.Count);
+
+            // Missing last param
+            args = new string[] { "--retries" }; // <-- no param!
+            Assert.ThrowsException<ParseException>(() => Parser.Parse(args, expectedOptions));
+        }
+
+        [TestMethod]
+        public void TestExampleUsage()
+        {
+            string[] args = new string[] { "source", "dest", "-v", "-i", "a.txt", "b.txt", "c.txt", "--timeout", "30" };
+            CopyFilesTool.Main_(args);
         }
     }
 }
